cd40686e9a2249e0f192d02c28d34c659781ede9
Radhakamisetty <radhakamisetty@gmail.com>
2021-09-13 12:52:05 -0500

    add taskManager

--- js/index.js
+++ js/index.js
@@ -1,7 +1,23 @@
+import {TaskManager} from "./taskManager.js";
+
+const taskManager = new TaskManager();
+console.log(taskManager.tasks);
+
+taskManager.addTask( 'Take out the trash',
+ 'Take out the trash to the front of the house',
+ 'Nick',
+ '2020-09-20'
+ )
+ taskManager.addTask( 'Cook Dinner',
+ 'Take out the trash to the front of the house',
+ 'Nick',
+ '2020-09-20'
+ )
+ console.log(taskManager.tasks);
 const newTaskForm = document.querySelector("#task-form");
 
 function validFormFieldInput(data) {
-
+    let valid = true;
     const newTaskNameInput = document.querySelector("#task-form-name");
     const name = newTaskNameInput.value;
     console.log("name: " + name);
@@ -20,30 +36,34 @@ function validFormFieldInput(data) {
 
     if (name.length < 1){
         console.log('name is not valid');
-
+        valid = false;
     }
     if (assigned.length < 1){
         console.log('Assigned is not valid');
-
+        valid = false;
 
     }
 
     if (!dueDate){
         console.log('Due Date is not valid');
+        valid = false;
     }
-
-    if (!newTaskForm.checkValidity()) {
-        data.preventDefault()
-        data.stopPropagation()
-      }
-
-      newTaskForm.classList.add('was-validated')
-  
-
+ 
+    if (valid) {
+        taskManager.addTask(name, description, assigned, dueDate);
+        console.log(taskManager.tasks);
+        newTaskForm.reset();
+        newTaskForm.classList.remove('was-validated');
+    } 
 }
 
-
-
-newTaskForm.addEventListener('submit', validFormFieldInput)
+newTaskForm.addEventListener('submit', (event) => {
+    if (!newTaskForm.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+    }
+    newTaskForm.classList.add('was-validated')
+    validFormFieldInput()
+}, false) 
 
 
