0d167d74bfd192c08ce344f4c566b00119186709
Kassahun Hailemariam <kassahun.zhailemariam@gmail.com>
2021-09-13 17:05:43 -0500

    add display task

--- js/taskManager.js
+++ js/taskManager.js
@@ -1,3 +1,29 @@
+// task 6
+
+export const createTaskHtml = (id, name, description, assignedTo, dueDate, status) => {
+    const html = `<div class="card task-card">
+    <div class="card-header">
+      @${assignedTo}
+      <div class="float-end badge bg-success rounded-pill">${status}</div>
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">${id} ${name}</h5>
+      <p class="card-text">${description}</p>
+    </div>     
+    <div class="card-footer" >
+      <span>Due: ${dueDate}</span>
+      <div class="float-end">
+        <a href="#" class="btn btn-primary btn-sm d-none"><i class="bi bi-check-lg"></i> Complete</a>
+        <a href="#" class="btn btn-secondary btn-sm"><i class="bi bi-pencil"></i> Edit</a>
+        <a href="#" class="btn btn-danger btn-sm"><i class="bi bi-trash"></i> Delete</a>
+      </div>
+    </div>
+  </div>`
+  return html;
+}
+
+
+
 export class TaskManager {
     //currentId = 0;
     constructor(currentId) {
@@ -18,5 +44,23 @@ export class TaskManager {
         this.tasks.push(newTask);
     }
 
-    
+    render() {
+        const tasksHtmlList = [];
+        this.tasks.forEach(task => {
+            // const formattedDate = `${task.dueDate.}`
+            let options = {
+                year: 'numeric', month: 'numeric', day: 'numeric'
+            }
+            const formattedDate = new Intl.DateTimeFormat('en-US', options).format(task.dueDate);
+            const taskHtml = createTaskHtml(task.id, task.name, task.description, task.assignedTo, formattedDate, task.status);
+            tasksHtmlList.push(taskHtml);
+        })
+        const tasksHtml = tasksHtmlList.join('\n');
+        const tasksList = document.querySelector("#task-list");
+        // tasksList.innerHTML = tasksHtml;
+        tasksList.innerHTML = 'tasksHtml';
+        console.log(tasksHtml);
+        console.log(JSON.stringify(this.tasks))
+    }
 }
+
