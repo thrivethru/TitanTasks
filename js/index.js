4814addafe6d72f1a6e58c448cd59bf12ab12556
Mark Warner <4764698+thrivethru@users.noreply.github.com>
2021-09-17 17:31:27 -0500

    Add localStorage persistence, delete, status filter, update dueDate, and update assignedTo

--- js/index.js
+++ js/index.js
@@ -3,22 +3,55 @@ import { TaskManager } from "./taskManager.js";
 const taskManager = new TaskManager();
 const newTaskForm = document.querySelector("#task-form");
 const newTaskFormClear = document.querySelector("#task-form-clear");
+const tasksListFilter = document.querySelector("#task-list-radio-group");
 const tasksList = document.querySelector("#task-list");
+const taskFormDueDate = document.querySelector("#task-form-duedate");
+
+taskManager.load();
+const initialFilter = taskManager.getFilter();
+const filterQuery = `input[value="${initialFilter}"]`;
+tasksListFilter.querySelector(filterQuery).setAttribute("checked", "");
+taskManager.render();
 
 function validFormFieldInput(newTaskInput) {
   let { name, assignedTo, dueDate } = newTaskInput;
   let valid = true;
-
   if (name.length < 1) {
     valid = false;
   }
   if (assignedTo.length < 1) {
     valid = false;
   }
+  if (!validateDate(dueDate)) {
+    valid = false;
+    taskFormDueDate.setCustomValidity("customError");
+  } else {
+    taskFormDueDate.setCustomValidity("");
+  }
+  return valid;
+}
+
+function validateDate(dueDate) {
+  let valid = true;
   if (!dueDate) {
     valid = false;
   }
-
+  try {
+    let date = new Date(dueDate);
+    date.setTime(date.getTime() + date.getTimezoneOffset() * 60000);
+    const today = new Date(Date.now());
+    today.setHours(0);
+    today.setMinutes(0);
+    today.setSeconds(0);
+    today.setMilliseconds(0);
+    let year2100 = 4102444800000;
+    if (today > date || date >= year2100) {
+      valid = false;
+    }
+  } catch (error) {
+    // console.log(error);
+    valid = false;
+  }
   return valid;
 }
 
@@ -32,6 +65,7 @@ function processNewTask() {
 
   if (validFormFieldInput(newTaskInput)) {
     const dueDate = new Date(newTaskInput.dueDate);
+    dueDate.setTime(dueDate.getTime() + dueDate.getTimezoneOffset() * 60000);
     taskManager.addTask(
       newTaskInput.name,
       newTaskInput.description,
@@ -39,8 +73,56 @@ function processNewTask() {
       dueDate
     );
     taskManager.render();
+    taskManager.save();
     resetTaskForm();
   }
+}
+
+function updateStatus(event) {
+  const parentTask = event.target.parentNode.parentNode.parentNode.parentNode;
+  const taskId = parseInt(parentTask.dataset.taskId);
+  taskManager.setStatus(taskId);
+  taskManager.render();
+  taskManager.save();
+}
+
+function updateDate(event) {
+  const inputDate = event.target.value;
+  const parentTask =
+    event.target.parentNode.parentNode.parentNode.parentNode.parentNode;
+  const taskId = parseInt(parentTask.dataset.taskId);
+  if (validateDate(inputDate)) {
+    const newDueDate = new Date(inputDate);
+    newDueDate.setTime(
+      newDueDate.getTime() + newDueDate.getTimezoneOffset() * 60000
+    );
+    taskManager.setDate(taskId, newDueDate);
+    taskManager.save();
+  } else {
+    // TODO show validation
+  }
+  taskManager.render();
+}
+
+function updateAssignedTo(event) {
+  const inputAssingedTo = event.target.value;
+  const parentTask =
+    event.target.parentNode.parentNode.parentNode.parentNode.parentNode;
+  const taskId = parseInt(parentTask.dataset.taskId);
+  taskManager.setAssignedTo(taskId, inputAssingedTo);
+  taskManager.render();
+  taskManager.save();
+}
+
+function deleteTask(event) {
+  let parentTask = event.target.parentNode.parentNode.parentNode.parentNode;
+  if (event.target.classList.contains("delete-button-icon")) {
+    parentTask = parentTask.parentNode;
+  }
+  const taskId = parseInt(parentTask.dataset.taskId);
+  taskManager.delete(taskId);
+  taskManager.render();
+  taskManager.save();
 }
 
 function resetTaskForm() {
@@ -69,15 +151,34 @@ newTaskFormClear.addEventListener(
   false
 );
 
+tasksListFilter.addEventListener("click", (event) => {
+  const checked = event.target.value;
+  taskManager.setFilter(checked);
+  taskManager.render();
+  taskManager.save();
+});
+
 tasksList.addEventListener(
   "click",
   (event) => {
     if (event.target.classList.contains("done-button")) {
-      const parentTask = event.target.parentNode.parentNode.parentNode;
-      const taskId = parseInt(parentTask.dataset.taskId); 
-      const task = taskManager.getTaskById(taskId);
-      task.status = "Done";
-      taskManager.render();
+      updateStatus(event);
+    }
+    if (event.target.classList.contains("delete-button")) {
+      deleteTask(event);
+    }
+  },
+  false
+);
+
+tasksList.addEventListener(
+  "change",
+  (event) => {
+    if (event.target.classList.contains("task-card-date")) {
+      updateDate(event);
+    }
+    if (event.target.classList.contains("task-card-assign")) {
+      updateAssignedTo(event);
     }
   },
   false
